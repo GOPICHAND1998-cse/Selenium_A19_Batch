ffde67a0373140f61a8af54d79426e2652399264
Pritam <ijiopritam@gmail.com>
2026-08-13 14:00:30 +0530

    DDT Commit 2

--- src/test/java/ddtpackage/ReadDataFromProperty.java
+++ src/test/java/ddtpackage/ReadDataFromProperty.java
@@ -1,11 +1,14 @@
 package ddtpackage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.time.Duration;
 import java.util.Properties;
 
 public class ReadDataFromProperty
@@ -48,7 +51,31 @@ public class ReadDataFromProperty
 
             driver.get(url);
 
+            driver.manage().window().maximize();
 
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+            driver.findElement(By.xpath("//input[@id='user-name']"))
+                    .sendKeys(user);
+
+            driver.findElement(By.xpath("//input[@id='password']"))
+                    .sendKeys(password);
+
+            driver.findElement(By.xpath("//input[@id='login-button']"))
+                    .click();
+
+            driver.findElement(By.xpath("//div[text()='Sauce Labs Backpack']"))
+                    .click();
+
+            String selectedProductName = driver.findElement(By.xpath("//div[@data-test='inventory-item-name']"))
+                    .getText();
+
+            FileOutputStream fos = new FileOutputStream("./src/test/resources/CommonDataFolder/Config.properties");
+
+            property.setProperty("ProductName",selectedProductName);
+
+            property.store(fos,"Updated by SDET Pritam");
 
         }
         catch (Exception e)
